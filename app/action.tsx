3010c2ea1b19b51205dc554abb1e55a81b9e5621
= <gaurav.m@orocorp.in>
2024-08-31 16:08:20 +0530

    initial commit

---
+++ app/action.tsx
@@ -0,0 +1,24 @@
+"use server";
+
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+
+export const fetchAnime = async (
+  {
+    page,
+    limit,
+  }: {
+    page?: number | undefined;
+    limit?: number | undefined;
+  } = {
+    page: 1,
+    limit: 20,
+  }
+) => {
+  const response = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=${limit}`
+  );
+  const data = await response.json();
+  return data.map((item: AnimeProp, index: number) => (
+    <AnimeCard key={item.id} anime={item} index={index} />
+  ));
+};
